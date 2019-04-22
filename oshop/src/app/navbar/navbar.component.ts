0aca205734f4c0d08589852a088824273c41d3c4
David Wickizer <dwickizer@geocent.com>
2019-04-22 16:57:18 -0400

    Added basic google auth login

--- oshop/src/app/navbar/navbar.component.ts
+++ oshop/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth'
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'navbar',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  user: firebase.User;
+
+  constructor(private afAuth: AngularFireAuth) { }
 
   ngOnInit() {
+    this.afAuth.authState.subscribe(user => this.user = user);
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
